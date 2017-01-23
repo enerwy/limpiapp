48e88a3ae7821cd71d314c214b48c755a5cdb988
Ana Maria Sativa <anamaria.sativa@enerwygroup.com>
2017-01-23 10:33:43 -0500

    prueba ana V1.0

--- app/src/main/java/co/com/enerwygroup/www/limpiapp/MainActivity.java
+++ app/src/main/java/co/com/enerwygroup/www/limpiapp/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "prueba anita", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "prueba 2 anita", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-        System.out.println("ome gonorrea Omeeee!!");
+        System.out.println("depresion absoluta!!");
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
